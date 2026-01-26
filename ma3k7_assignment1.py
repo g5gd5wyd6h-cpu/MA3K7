7d8cdf79d892aa7bde02e519d57a1d3464c4fd48
Daniel Angus <g5gd5wyd6h@privaterelay.appleid.com>
2026-01-26 00:06:25 +0000

    Add files via upload

---
+++ ma3k7_assignment1.py
@@ -0,0 +1,24 @@
+def step(a,b):
+    return b, (a + b) % 10
+
+def encode_state(a,b):
+    return 10*a + b
+
+canonical_states = []
+
+for a in range(0,10):
+    for b in range(0,10):
+
+        #re-initialize collisions for each (a,b)
+        collisions = []
+
+        while True:
+            state = encode_state(a,b)
+            if state in collisions:
+                canonical_states.append(min(collisions))
+                break
+            else:
+                collisions.append(state)
+                a,b = step(a,b) 
+            
+print("Number of distinct canonical states:", sorted(set(canonical_states)))
